af422561ddab04cddba7113935767162faace96d
Luciano dos Santos <luciano_z7@hotmail.com>
2018-03-31 13:59:08 -0300

    Created welcome page.

---
+++ src/pages/welcome/welcome.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-welcome',
+  templateUrl: 'welcome.html'
+})
+export class WelcomePage {
+
+  constructor() {
+
+  }
+
+}
