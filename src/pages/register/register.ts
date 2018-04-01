79757e0184d4e419aaa18896dbf75d63f4d7cf76
Rafael Makaha Gomes Ferreira <rafael.makaha@gmail.com>
2018-04-01 13:46:20 -0300

    Created register page.

---
+++ src/pages/register/register.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-register',
+  templateUrl: 'register.html'
+})
+export class RegisterPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
