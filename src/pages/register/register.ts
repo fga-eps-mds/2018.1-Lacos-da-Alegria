b086547e01e7f52f7a05e9d9474342a0a9b1e65c
Luiz Guilherme Silva <do.guilherme@hotmail.com>
2018-04-04 14:32:07 -0300

    Added karma, jasmine, tslint and travis ci integration on project

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, group } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LoginPage } from '../login/login';
