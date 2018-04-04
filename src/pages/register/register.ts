ffcd01ba86335300531a946b4fdcbf6402478b16
Luciano dos Santos <luciano_z7@hotmail.com>
2018-04-04 00:09:01 -0300

    Validated registration fields.

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,17 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, group } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoginPage } from '../login/login';
+import { EmailValidator } from '../../validators/email';
 
 @Component({
   selector: 'page-register',
   templateUrl: 'register.html'
 })
 export class RegisterPage {
+  @ViewChild('signup') signup: any;
+  signupOne: FormGroup;
+  submitAttemp: boolean = false;
+  constructor(public navController: NavController,  public formBuilder: FormBuilder) {
+    this.signupOne = formBuilder.group({
+      nameFull: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z]*'), Validators.required])],
+      email: ['', Validators.compose([Validators.required, EmailValidator.isValid])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(12), Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{6,12}$')])],
+      confirmPassword: ['', Validators.required, Validators],
+    }, { validator: this.matchingPasswords('password', 'confirmPassword')})
+  }
+  matchingPasswords(passwordKey: string, confirmPasswordKey: string){
+    return (group: FormGroup): {[key: string]: any} => {
+      let password = group.controls[passwordKey];
+      let confirmPassword = group.controls[confirmPasswordKey];
 
-  constructor(public navController: NavController) {
-
+      if(password.value !== confirmPassword.value){
+        return {
+          mismatchedPasswords: true
+        };
+      }
+    }
   }
   BtnSend(){
-    
+    this.submitAttemp = true;
+
+    if(!this.signupOne.valid){
+      this.navController.push(LoginPage);
+    }
+    else{
+      console.log("Success!");
+      console.log(this.signupOne.value);
+    }
   }
 
 }
