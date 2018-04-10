8aea9d53cf65bcbba22e88be73cc1504b9a623e5
Sara Silva <saracsas2@gmail.com>
2018-04-09 23:57:32 -0300

    Created activities list page.
    Co-authored-by: Rafael Makaha <rafael.makaha@gmail.com>

---
+++ src/pages/activities-list/activities-list.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-activities-list',
+  templateUrl: 'activities-list.html'
+})
+export class ActivitiesListPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
