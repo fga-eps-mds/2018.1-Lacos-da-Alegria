1bc436a18a7c2d6ad13079b6399f9b0c0a7a9caa
Rafael Makaha Gomes Ferreira <rafael.makaha@gmail.com>
2018-04-10 23:55:17 -0300

    Implemented list activities functionality
    Co-authored-by: Renan Schadt <welzrenan@gmail.com>
    Co-authored-by: Sara Silva <saracsas2@gmail.com>

--- src/pages/activities-list/activities-list.ts
+++ src/pages/activities-list/activities-list.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { RestApiProvider } from '../../providers/rest-api/rest-api';
 
 @Component({
   selector: 'page-activities-list',
   templateUrl: 'activities-list.html'
 })
 export class ActivitiesListPage {
+  activities: any;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public restApiProvider: RestApiProvider) {
+    this.getActivities();
   }
 
+  getActivities() {
+    this.restApiProvider.getActivities()
+    .then(data => {
+      this.activities = data;
+      console.log(this.activities);
+    });
+  }
 }
