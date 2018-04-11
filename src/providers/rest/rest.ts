a7e49bf7652f2acadfc51aea9511bbac9aad5a1b
Rômulo Vinícius de Souza <romulovinicius10@gmail.com>
2018-04-10 21:28:26 -0300

    Added getListUsers method in rest.ts.
    Signed-off-by: Rômulo Vinícius de Souza <romulovinicius10@gmail.com>
    Signed-off-by: João Vítor Morandi Lemos <joao.lemos4000@gmail.com>
    Signed-off-by: Luciano dos Santos <luciano_z7@hotmail.com>
    
    Co-authored-by: João Vítor Morandi Lemos <joao.lemos4000@gmail.com>
    Co-authored-by: Luciano dos Santos <luciano_z7@hotmail.com>
    Co-authored-by: Rômulo Vinícius de Souza <romulovinicius10@gmail.com>

--- src/providers/rest/rest.ts
+++ src/providers/rest/rest.ts
@@ -10,7 +10,8 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class RestProvider {
   apiUrl = 'https://jsonplaceholder.typicode.com';
-  localApiUrl = 'http://localhost:8000/api'
+  localApiUrl = 'http://localhost:8000/api';
+  fakeUrl = 'https://jsonplaceholder.typicode.com';
 
   constructor(public http: HttpClient) {
     console.log('Hello RestProvider Provider');
@@ -19,6 +20,16 @@ export class RestProvider {
   getUsers() {
     return new Promise(resolve => {
       this.http.get(this.apiUrl+'/users').subscribe(data => {
+        resolve(data);
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+
+  getListUsers(){
+    return new Promise(resolve => {
+      this.http.get(this.fakeUrl+'/users').subscribe(data => {
         resolve(data);
       }, err => {
         console.log(err);
