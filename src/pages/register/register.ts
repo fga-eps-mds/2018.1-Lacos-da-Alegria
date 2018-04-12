3e045e4db3d9897ee41a57e4b72c41d36e2bdd93
Rômulo Vinícius de Souza <romulovinicius10@gmail.com>
2018-04-11 21:13:29 -0300

    Adapted user registration to correct http post.

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -11,40 +11,43 @@ import { RestProvider } from '../../providers/rest/rest'
 })
 export class RegisterPage {
   @ViewChild('signup') signup: any;
-  signupOne: FormGroup;
   submitAttemp: boolean = false;
-  user = { name: '', address: '', email: '', password: ''};
-  constructor(public navController: NavController,  public formBuilder: FormBuilder, public restProvider: RestProvider) {
-    this.signupOne = formBuilder.group({
-      login: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z]*'), Validators.required])],
-      nameFull: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z]*'), Validators.required])],
-      cpf: ['', Validators.compose([Validators.required, Validators.minLength(11), Validators.maxLength(11), Validators.pattern('([0-9]).{11,11}$')])],
-      email: ['', Validators.compose([Validators.required, EmailValidator.isValid])],
-      birth: ['', Validators.compose([Validators.required, Validators.pattern('^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$')])],
-      cities:[''],
-      ddd: ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(3), Validators.pattern('([0-9]).{3,3}$')])],
-      hospitais:[''],
-      whatsapp: ['', Validators.compose([Validators.required, Validators.minLength(9), Validators.maxLength(9), Validators.pattern('([0-9]).{9,9}$')]) ],
-      endereco: ['',Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z]*'), Validators.required]) ],
-      comoConheceu:[''],
-      participar:[''],
-      sexo:[''],
-      password: ['', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(12), Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{6,12}$')])],
-      confirmPassword: ['', Validators.required],
-    }, { validator: this.matchingPasswords('password', 'confirmPassword')})
-  }
-  matchingPasswords(passwordKey: string, confirmPasswordKey: string){
-    return (group: FormGroup): {[key: string]: any} => {
-      let password = group.controls[passwordKey];
-      let confirmPassword = group.controls[confirmPasswordKey];
+  user = { name: '', email: '', address: '', password: ''};
 
-      if(password.value !== confirmPassword.value){
-        return {
-          mismatchedPasswords: true
-        };
-      }
-    }
+  constructor(public navCtrl: NavController, public restProvider: RestProvider) {
+  
   }
+  // constructor(public navController: NavController,  public formBuilder: FormBuilder, public restProvider: RestProvider) {
+  //   this.signupOne = formBuilder.group({
+  //     login: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z]*'), Validators.required])],
+  //     nameFull: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z]*'), Validators.required])],
+  //     cpf: ['', Validators.compose([Validators.required, Validators.minLength(11), Validators.maxLength(11), Validators.pattern('([0-9]).{11,11}$')])],
+  //     email: ['', Validators.compose([Validators.required, EmailValidator.isValid])],
+  //     birth: ['', Validators.compose([Validators.required, Validators.pattern('^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$')])],
+  //     cities:[''],
+  //     ddd: ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(3), Validators.pattern('([0-9]).{3,3}$')])],
+  //     hospitais:[''],
+  //     whatsapp: ['', Validators.compose([Validators.required, Validators.minLength(9), Validators.maxLength(9), Validators.pattern('([0-9]).{9,9}$')]) ],
+  //     endereco: ['',Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z]*'), Validators.required]) ],
+  //     comoConheceu:[''],
+  //     participar:[''],
+  //     sexo:[''],
+  //     password: ['', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(12), Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{6,12}$')])],
+  //     confirmPassword: ['', Validators.required],
+  //   }, { validator: this.matchingPasswords('password', 'confirmPassword')})
+  // }
+  // matchingPasswords(passwordKey: string, confirmPasswordKey: string){
+  //   return (group: FormGroup): {[key: string]: any} => {
+  //     let password = group.controls[passwordKey];
+  //     let confirmPassword = group.controls[confirmPasswordKey];
+
+  //     if(password.value !== confirmPassword.value){
+  //       return {
+  //         mismatchedPasswords: true
+  //       };
+  //     }
+  //   }
+  // }
   saveUser() {
     this.restProvider.saveUser(this.user).then((result) => {
       console.log(result);
