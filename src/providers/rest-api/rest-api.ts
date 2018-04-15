1915455ed373bc5442e8765089d2a7c14935dc91
Sara Silva <saracsas2@gmail.com>
2018-04-14 23:53:53 -0300

    Fixed rest-api providers. Signed-off-by: Joao Lemos <joao.lemos400@gmail.com>
    Co-authored-by: Joao Lemos <joao.lemos400@gmail.com>

--- src/providers/rest-api/rest-api.ts
+++ src/providers/rest-api/rest-api.ts
@@ -1,24 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-/*
-  Generated class for the RestApiProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class RestApiProvider {
   private apiUrl = 'http://localhost:8000/api/';
 
-
   constructor(public http: HttpClient) {
-    console.log('Hello RestApiProvider Provider');
+    console.log('Hello RestProvider Provider');
   }
 
-
-  getActivities(){
-    return new Promise(resolve=>{
+  getActivitiesList(){
+    return new Promise(resolve => {
       this.http.get(this.apiUrl+'/activities').subscribe(data => {
         resolve(data);
       }, err => {
