7a6298982273f1519d5ac40ba09a193d02c2f3fd
joaovitorml <joao.lemos4000@gmail.com>
2018-04-30 19:46:19 -0300

    Tested some functions with jasmine. Signed-off-by: Rômulo Vinícius de Souza <romulovinicius10@gmail.com> Signed-of-br: João Vítor Morandi <joao.lemos4000@gmail.com>
    Co-authored-by: Rômulo Vinícius de Souza <romulovinicius10@gmail.com>
    Co-authored-by: João Vítor Morandi <joao.lemos4000@gmail.com>

--- spec/profile.spec.ts
+++ spec/profile.spec.ts
@@ -1,6 +1,21 @@
+
+
 describe("testando",function(){
+
+  // var ProfilePage = require('../src/pages/profile/profile.ts');
+  // var NavController;
+  // var construtor;
+  //
+  // beforeEach(function(){
+  //   construtor = new ProfilePage();
+  // });
+
   it("teste",function(){
     expect(1).toBe(1);
   });
+
+   // it("constructor",function(){
+   //   ProfilePage.constructor(NavController);
+   // });
 
 });
