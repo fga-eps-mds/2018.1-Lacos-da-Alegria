23b92ebdbb3289ac0b6940f37dd3bf3ee2a7f937
Rômulo Vinícius de Souza <romulovinicius10@gmail.com>
2018-05-04 13:13:06 -0300

    Added profile's unit test.
    Signed-off-by: Rômulo Vinícius de Souza <romulovinicius10@gmail.com>
    Signed-off-by: Joao Vitor Lemos <joao.lemos4000@gmail.com>
    
    Co-authored-by: Joao Vitor Lemos <joao.lemos4000@gmail.com>
    Co-authored-by: Rômulo Vinícius de Souza <romulovinicius10@gmail.com>

---
+++ src/pages/profile/profile.spec.ts
@@ -0,0 +1,34 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By }           from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { ProfilePage } from './profile';
+import { IonicModule, Platform, NavController} from 'ionic-angular/index';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+describe('Profile', () => {
+  let de: DebugElement;
+  let comp: ProfilePage;
+  let fixture: ComponentFixture<ProfilePage>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ProfilePage],
+      imports: [
+        IonicModule.forRoot(ProfilePage)
+      ],
+      providers: [
+        NavController
+      ]
+    });
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProfilePage);
+    comp = fixture.componentInstance;
+    de = fixture.debugElement.query(By.css('h3'));
+  });
+  it('should be created', () => {
+    expect(comp instanceof ProfilePage).toBe(true);
+  });
+});
