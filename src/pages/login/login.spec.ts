f860b1bdb35b6d4509eec17524e57138836708c1
Rômulo Vinícius de Souza <romulovinicius10@gmail.com>
2018-05-04 13:15:43 -0300

    Changed test files.
    Signed-off-by: Rômulo Vinícius de Souza <romulovinicius10@gmail.com>
    Signed-off-by: Joao Vitor Lemos <joao.lemos4000@gmail.com>
    
    Co-authored-by: Joao Vitor Lemos <joao.lemos4000@gmail.com>
    Co-authored-by: Rômulo Vinícius de Souza <romulovinicius10@gmail.com>

---
+++ src/pages/login/login.spec.ts
@@ -0,0 +1,30 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By }           from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { LoginPage } from './login';
+import { IonicModule, Platform, NavController} from 'ionic-angular/index';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+describe('Login', () => {
+  let de: DebugElement;
+  let comp: LoginPage;
+  let fixture: ComponentFixture<LoginPage>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [LoginPage],
+      imports: [
+        IonicModule.forRoot(LoginPage)
+      ],
+      providers: [
+        NavController
+      ]
+    });
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginPage);
+    comp = fixture.componentInstance;
+    de = fixture.debugElement.query(By.css('h3'));
+  });
