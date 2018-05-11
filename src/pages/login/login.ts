ee369dc4bf0227422f4b28598c056c7c8241d44e
Rômulo Vinícius de Souza <romulovinicius10@gmail.com>
2018-05-11 19:54:34 -0300

    Edited userLogin method in login.ts
    Signed-off-by: Rômulo Vinícius de Souza <romulovinicius10@gmail.com>
    Signed-off-by: Joao Vitor Lemos <joao.lemos4000@gmail.com>
    
    Co-authored-by: Joao Vitor Lemos <joao.lemos4000@gmail.com>
    Co-authored-by: Rômulo Vinícius de Souza <romulovinicius10@gmail.com>

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -4,6 +4,7 @@ import { NavController } from 'ionic-angular';
 import { RegisterPage } from '../register/register';
 import { RestProvider } from '../../providers/rest/rest';
 import { HomePage } from '../home/home';
+import { StorageService } from '../../providers/storage.service';
 
 @Component({
   selector: 'page-login',
@@ -11,7 +12,7 @@ import { HomePage } from '../home/home';
 })
 export class LoginPage {
   user = { username:'', password:''}
-  constructor(public navController: NavController,  public restProvider: RestProvider) {
+  constructor(public navController: NavController,  public restProvider: RestProvider, public storage: StorageService) {
 
   }
   BtnRegister(){
@@ -32,21 +33,28 @@ export class LoginPage {
     this.restProvider.authenticate(this.user)
       .subscribe(response => {
         //this.restProvider.successfulLogin(response.headers.get('Authorization'));
-        console.log(response.body);
-        let data: LocalUser = {
-          refreshToken:'',
-          accessToken:'',
-          username:'',
-        };
-        data.refreshToken = response.body.substr(11,209);
-        data.accessToken = response.body.substr(230,207);
-        console.log('refreeeeesh',data.refreshToken);
-        console.log('acessoooooooo', data.accessToken);
+        // console.log(response.body);
+        // let data: LocalUser  = {
+        //   refreshToken:'',
+        //   accessToken:'',
+        //   username:'',
+        // };
+        let refreshToken = response.body.substr(11,209);
+        let accessToken = response.body.substr(230,207);
+        let username = this.user.username;
+        console.log('refreeeeesh',refreshToken);
+        console.log('acessoooooooo',accessToken);
         console.log('response',response);
+        this.storage.setLocalUser(username, accessToken, refreshToken);
+        // this.storage.setLocalUser(null);
+        // let data2 = this.storage.getLocalUser();
+        // console.log('data2 = ', data2);
         this.navController.push(HomePage);
+        
       },
       error => {
         console.log(error);
       });
   }
+
 }
