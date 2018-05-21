6f862a7e907b11735321349977164c329cbc9486
Lucas <lucasoliveira2306@gmail.com>
2018-05-21 18:19:27 -0300

    Added login acceptance tests.
    Signed-off-by: Luciano Santos <luciano_z7@hotmail.com>
    Signed-off-by: Lucas Oliveira <lucasoliveira@gmail.com>
    
    Co-authored-by: Luciano Santos <luciano_z7@hotmail.com>
    Co-authored-by: Lucas Oliveira <lucasoliveira@gmail.com>

--- src/pages/login/login.e2e.spec.ts
+++ src/pages/login/login.e2e.spec.ts
@@ -1,15 +1,21 @@
 import { browser, element, by } from 'protractor';
 
-
 var user;
 
 describe('Login tests', () =>{
+  // Run before each case test
   beforeEach(() => {
     browser.get('');
-    browser.driver.sleep(9500);
+    element(by.xpath('//*[@data-slide-index="3"]')).click();
+    browser.driver.sleep(500);
+    element(by.xpath('/html/body/ion-app/ng-component/ion-nav/page-welcome/ion-content/div[2]/ion-slides/div/div[1]/ion-slide[4]/div/div/button')).click();
+
+    
+
+    browser.driver.sleep(500);
   });
 
-  it('test', () =>{
-    browser.driver.sleep(9500);
+  it('Login succesful', () =>{
+    browser.driver.sleep(500);
   });
 });
