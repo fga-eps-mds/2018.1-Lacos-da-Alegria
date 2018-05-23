504a877dbce437ba30b838c9bc2972ab4c068f97
Luciano Santos <luciano_z7@hotmail.com>
2018-05-22 23:19:33 -0300

    Created function file for tests and created beforeEach for login.
    Signed-off-by: Luciano Santos <luciano_z7@hotmail.com>
    Signed-off-by: Lucas Oliveira <lucasoliveira2306@gmail.com>
    
    Co-authored-by: Lucas Oliveira <lucasoliveira2306@gmail.com>

--- src/pages/login/login.e2e.spec.ts
+++ src/pages/login/login.e2e.spec.ts
@@ -1,18 +1,51 @@
 import { browser, element, by } from 'protractor';
+import { fillSignupFields, clickLastSlide, clickEntryButton, clickSignUpButton, clickNextButton, clickNextButton_2, clickDateFieldButton, fillSignupFields_2, clickGenreFieldButton, fillSignupFields_3 , clickRegisterButton} from './loginFunctions';
 
 var user;
 
 describe('Login tests', () =>{
   // Run before each case test
   beforeEach(() => {
+
+    user = {
+      username: 'renatinho',
+      name: 'renato vinicius',
+      email: 'renatinho@gmail.com',
+      password:'abc123456',
+      passwordVerify: 'abc123456',
+      cpf: '05730259166',
+      endereco: 'quadra 14',
+      ddd: '061',
+      whatsapp: '993675487'
+    };
+
     browser.get('');
-    element(by.xpath('//*[@data-slide-index="3"]')).click();
-    browser.driver.sleep(500);
-    element(by.xpath('/html/body/ion-app/ng-component/ion-nav/page-welcome/ion-content/div[2]/ion-slides/div/div[1]/ion-slide[4]/div/div/button')).click();
-
-    
 
     browser.driver.sleep(500);
+    clickLastSlide();
+    browser.driver.sleep(500);
+    clickEntryButton();
+    browser.driver.sleep(500);
+    clickSignUpButton();
+    browser.driver.sleep(500);
+    fillSignupFields(user);
+    browser.driver.sleep(500);
+    clickNextButton();
+    browser.driver.sleep(500);
+    fillSignupFields_2(user);
+    browser.driver.sleep(500);
+    clickDateFieldButton();
+    browser.driver.sleep(500);
+    clickGenreFieldButton();
+    browser.driver.sleep(500);
+    clickNextButton_2();
+    browser.driver.sleep(500);
+    fillSignupFields_3();
+    browser.driver.sleep(500);
+    clickRegisterButton();
+    browser.driver.sleep(500);
+
+
   });
 
   it('Login succesful', () =>{
--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -102,12 +102,13 @@ export class RegisterPage {
   }
 
   saveUser() {
-    this.restProvider.saveUser(this.getForm()).then((result) => {
+    this.btnGoToLogin();
+    /* this.restProvider.saveUser(this.getForm()).then((result) => {
        console.log("oieeee");
       this.navCtrl.push(LoginPage);
     }, (err) => {
       console.log(err);
-    });
+    }); */
   }
 
   showError(data, id: string){
