ba5fcad78d7bf840e44674a15752201e997b2688
Rômulo Vinícius de Souza <romulovinicius10@gmail.com>
2018-06-17 04:35:59 -0300

    Added functions to show the status of the user in activities.
    Signed-off-by: Luciano dos Santos <luciano_z7@hotmail.com>
    Signed-off-by: Rômulo Vinícius de Souza <romulovinicius10@gmail.com>
    Signed-off-by: João Vítor Morandi <joao.lemos4000@gmail.com>
    Signed-off-by: Renan Schadt <welzrenan@gmail.com>
    Signed-off-by: Sara Silva <saracsas2@gmail.com>
    
    Co-authored-by: Luciano dos Santos <luciano_z7@hotmail.com>
    Co-authored-by: João Vítor Morandi <joao.lemos4000@gmail.com>
    Co-authored-by: Renan Schadt <welzrenan@gmail.com>
    Co-authored-by: Sara Silva <saracsas2@gmail.com>

--- src/pages/activities-list/activities-list.ts
+++ src/pages/activities-list/activities-list.ts
@@ -5,6 +5,7 @@ import { RoleService } from '../../providers/role.service';
 import { RestActivityProvider } from '../../providers/rest-activity';
 
 import { ActivityDetailsPage } from '../activity-details/activity-details';
+import { RestUserProvider } from '../../providers/rest-user';
 
 @Component({
   selector: 'page-activities-list',
@@ -17,15 +18,53 @@ export class ActivitiesListPage {
   aux: any;
   indexes: any;
   role: any;
+  id: any;
+  resposta: string;
+  // array[number, string];
+  array = new Array(10);
+  contador = 0;
 
   constructor(
     public modalCtrl: ModalController,
     public navCtrl: NavController,
     public restProvider: RestActivityProvider,
+    public restUserProvider: RestUserProvider,
     public roleService: RoleService) {
+      this.id = this.restUserProvider.getId();
+      this.restUserProvider.getUserActivitiesIds(this.id).subscribe((data: any)=>{
+        console.log('data = ', data);
+        for (let index = 0; index < data.aux.length; index++) {
+          console.log('data.aux index = ', data.aux[index]);
+          //data.aux[index] = id of activity
+          //this.id = id of user
+          this.restUserProvider.searchPosition(this.id, data.aux[index]).subscribe((resp: any)=>{
+            console.log('resp = ', resp); 
+            this.array[index] = [data.aux[index],resp.resp];
+          }, (error)=>{
+            console.log('error = ', error);
+          })
+          
+        }
+      }, (err) =>{
+        console.log('erro = ', err);
+      })
       this.getHospitalActivitiesList();
       this.getNGOActivitiesList();
       this.role = this.roleService.getLocalRole();
+      console.log('iiid = ', this.id);
+      // this.restUserProvider.getUserActivitiesIds(2);
+      
+  }
+  
+  getResp(id){
+    // console.log('id no get = ', id.id)
+    for (let index = 0; index < this.array.length; index++) {
+      if(this.array[index] && id.id == this.array[index][0]){
+        this.resposta = this.array[index][1];
+        return true;
+      }
+    }
+    return false;
   }
 
   openModal(index) {
