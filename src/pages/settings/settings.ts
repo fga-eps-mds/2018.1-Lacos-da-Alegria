1632778bf4f50e83811660eb7453fce71ea6864b
Luiz Guilherme Santos da Silva <x05603097167@tcu.gov.br>
2018-06-19 11:12:35 -0300

    Removed WelcomeModule

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-@IonicPage()
 @Component({
   selector: 'page-settings',
   templateUrl: 'settings.html',
