14a297c8f4fbc68cd87fc888022e0b3db7bd3436
Renan Schadt <welzrenan@gmail.com>
2018-06-23 22:40:05 -0300

    Added content to news page.
    Signed-off-by: Luciano dos Santos <luciano_z7@hotmail.com>
    Signed-off-by: João Vítor Morandi <joao.lemos4000@gmail.com>
    Signed-off-by: Renan Schadt <welzrenan@gmail.com>
    
    Co-authored-by: Luciano dos Santos <luciano_z7@hotmail.com>
    Co-authored-by: João Vítor Morandi <joao.lemos4000@gmail.com>

--- src/pages/news/news.ts
+++ src/pages/news/news.ts
@@ -1,18 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { RestNewsProvider } from '../../providers/rest-news';
+
 @Component({
   selector: 'page-news',
   templateUrl: 'news.html',
 })
 
 export class NewsPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  news_list: any;
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams, 
+    public restNewsProvider: RestNewsProvider) {
+      this.getNewsList()
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad NewsPage');
+  }
+
+  getNewsList(){
+    return this.restNewsProvider.getNewsList()
+    .then(data => {
+      this.news_list = data;
+    });
   }
 
 }
